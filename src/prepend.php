7ff13fc50a003d43b0c2838ea264dfd407935eda
Ryan <rsul.dev@me.com>
2014-07-04 20:27:18 -0400

    More big changes
    - Cleanup injected code
    	- Injected code now offers `imagePrevious` and `imageNext`
    - Work on copius theme
    	- Add title
    	- Add CSS reset
    	- Add jQuery
    	- Add JS to handle resizing
    	- Add JS to handle arrow keys
    	- Add JS to handle preloading next image

--- src/prepend.php
+++ src/prepend.php
@@ -1,6 +1,29 @@
 <?php
-$_gg_data = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'data.json'), true);
+$_gg_data_raw = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'data.json');
+$_gg_data = json_decode($_gg_data_raw, true);
+
 $images = $_gg_data['images'];
 $title = $_gg_data['title'];
-$image = isset($_GET['image']) && in_array($_GET['image'], $images) ? $_GET['image'] : null;
+
+if (isset($_GET['image']) && in_array($_GET['image'], $images))
+{
+	$image = $_GET['image'];
+}
+
+if ($image)
+{
+	$imageIndex = array_search($image, $images);
+
+	$imagePreviousIndex = $imageIndex - 1;
+	$imageNextIndex = $imageIndex + 1;
+
+	if (isset($images[$imagePreviousIndex]))
+	{
+		$imagePrevious = $images[$imagePreviousIndex];
+	}
+	if (isset($images[$imageNextIndex]))
+	{
+		$imageNext = $images[$imageNextIndex];
+	}
+}
 ?>
--- themes/copius/index.php
+++ themes/copius/index.php
@@ -9,11 +9,62 @@ if ( ! isset($image))
 <!DOCTYPE html>
 <html>
 <head>
+	<title><?=$title?></title>
 
+	<link rel="stylesheet" type="text/css" href="static/reset.css">
+	<script type="text/javascript" src="static/jquery-2.1.1.min.js"></script>
+
+	<script type="text/javascript">
+	imagePrevious = <?=json_encode(isset($imagePrevious) ? $imagePrevious : null)?>;
+	imageNext = <?=json_encode(isset($imageNext) ? $imageNext : null)?>;
+
+	function resizeImage()
+	{
+		$('#image').height($(window).height());
+	}
+
+	function gotoImage(image)
+	{
+		if ( ! image) return;
+		document.location = "?image=" + encodeURIComponent(image);
+	}
+
+	function preloadImage(url, callback) {
+		var img = new Image();
+		$(img).on('load', callback).attr('src', url);
+	}
+
+	$(document).ready(function()
+	{
+		// Handle resizing and making the image as big as possible
+		$(window).on('resize', function()
+		{
+			resizeImage();
+		});
+		resizeImage();
+
+		// Handle moving back/forth using keys
+		$(document).on('keyup', function(event)
+		{
+			if (event.which == 37)
+			{
+				gotoImage(imagePrevious);
+			}
+			else if (event.which == 39)
+			{
+				gotoImage(imageNext);
+			}
+		});
+
+		// Cache the surrounding images
+		preloadImage(imageNext);
+		preloadImage(imagePrevious);
+	});
+	</script>
 </head>
 <body>
 	<?php if (isset($image)): ?>
-		<img src="<?=$image?>">
+		<img id="image" src="<?=$image?>">
 	<?php endif; ?>
 </body>
 </html>
